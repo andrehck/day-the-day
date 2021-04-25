14da7df581f67e7e64d622048f8f34f68080d5e2
André Ferreira <54104227+andrehck@users.noreply.github.com>
2021-04-25 15:10:32 -0300

    Add files via upload

---
+++ cli.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	a := os.Args[1]
+	b := os.Args[2]
+
+	fmt.Println(a)
+	fmt.Println(b)
+}
+
+func help() {
+}
+
+func serviceid() {
+
+}
+
+func address() {
+
+}
