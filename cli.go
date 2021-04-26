3db0f660d024822972c2d72aa239d4422afb9e48
André Ferreira <andreferreiratrc@gmail.com>
2021-04-26 09:23:16 -0300

    if

--- cli.go
+++ cli.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 )
 
 func main() {
 	a := os.Args[1]
 	c := os.Args[2]
+	b := runtime.GOOS
 
 	fmt.Println(a)
 	fmt.Println(c)
+	fmt.Println(b)
+
+	if os.Args[1] == "1" {
+		fmt.Print("Ok")
+	} else {
+		fmt.Print("Fail")
+	}
 }
 
 func help() {
