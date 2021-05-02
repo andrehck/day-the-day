02e6eb1be71715df1593f8c9c85f1d503c779113
André Ferreira <andreferreiratrc@gmail.com>
2021-05-01 21:08:47 -0300

    test with for

---
+++ tdd-6/interacao_test.go
@@ -0,0 +1,12 @@
+package interacao
+
+import "testing"
+
+func TestRepetir(t *testing.T) {
+	repeticoes := Repetir("a")
+	esperado := "aaaaa"
+
+	if repeticoes =! esperado {
+		t.Errorf("esperado '%s' mas obteve '%s'", esperado, repeticoes)
+	}
+}
