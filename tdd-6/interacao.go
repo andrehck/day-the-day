e1c76636bb4803c9f558471b795a6a62bbde4000
André Ferreira <andreferreiratrc@gmail.com>
2021-05-01 22:13:37 -0300

    refatoração

--- tdd-6/interacao.go
+++ tdd-6/interacao.go
@@ -1,10 +1,12 @@
 package interacao
 
+const qtdRpt = 5
+
 func Repetir(a string) string {
 	var repeticoes string
 
-	for i := 0; i < 5; i++ {
-		repeticoes = repeticoes + a
+	for i := 0; i < qtdRpt; i++ {
+		repeticoes += a
 	}
 	return repeticoes
 }
