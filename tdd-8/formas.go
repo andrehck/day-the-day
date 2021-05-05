f926827ed016a340198bd57a2c5325cedf6804e1
André Ferreira <andreferreiratrc@gmail.com>
2021-05-05 08:54:29 -0300

    entendendo metodos

--- tdd-8/formas.go
+++ tdd-8/formas.go
@@ -4,6 +4,10 @@ func Perimetro(retangulo Retangulo) float64 {
 	return 2 * (retangulo.Largura + retangulo.Altura)
 }
 
-func Area(retangulo Retangulo) float64 {
-	return retangulo.Largura * retangulo.Altura
+func (r Retangulo) Area() float64 {
+	return 72.00
+}
+
+func (c Circulo) Area() float64 {
+	return 314.15
 }
--- tdd-8/formas_test.go
+++ tdd-8/formas_test.go
@@ -6,6 +6,9 @@ type Retangulo struct {
 	Largura float64
 	Altura  float64
 }
+type Circulo struct {
+	Raio float64
+}
 
 func TestPerimetro(t *testing.T) {
 	retangulo := Retangulo{10.0, 10.0}
@@ -18,11 +21,25 @@ func TestPerimetro(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	retangulo := Retangulo{12.0, 6.0}
-	resultado := Area(retangulo)
-	esperado := 72.0
+	t.Run("retangulos", func(t *testing.T) {
+		retangulo := Retangulo{12.0, 6.0}
+		resultado := retangulo.Area()
+		esperado := 72.0
 
-	if resultado != esperado {
-		t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
-	}
+		if resultado != esperado {
+			t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
+		}
+	})
+
+	t.Run("circulos", func(t *testing.T) {
+		circulo := Circulo{10}
+		resultado := circulo.Area()
+		esperado := 314.15
+
+		if resultado != esperado {
+			t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
+		}
+
+	})
+
 }
