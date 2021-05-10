62dffa4b59070f473e3175c290434e630a332e5b
André Ferreira <andreferreiratrc@gmail.com>
2021-05-10 11:07:47 -0300

    add retirada, sub testes

--- tdd-9/carteira_test.go
+++ tdd-9/carteira_test.go
@@ -4,22 +4,25 @@ import (
 	"testing"
 )
 
-type Bitcoin int
-
-type Carteira struct {
-	saldo Bitcoin
-}
-
 func TestCarteira(t *testing.T) {
-	carteira := Carteira{}
+	confirmaSaldo := func(t *testing.T, carteira Carteira, esperado Bitcoin) {
+		t.Helper()
+		resultado := carteira.Saldo()
 
-	carteira.Depositar(Bitcoin(10))
-
-	resultado := carteira.Saldo()
-
-	esperado := Bitcoin(10)
-
-	if resultado != esperado {
-		t.Errorf("resultado %d, esperado %d", resultado, esperado)
+		if resultado != esperado {
+			t.Errorf("resultado %s, esperado %s", resultado, esperado)
+		}
 	}
+
+	t.Run("Depositar", func(t *testing.T) {
+		carteira := Carteira{}
+		carteira.Depositar(Bitcoin(10))
+		confirmaSaldo(t, carteira, Bitcoin(10))
+	})
+
+	t.Run("Retirar", func(t *testing.T) {
+		carteira := Carteira{saldo: Bitcoin(20)}
+		carteira.Retirar(10)
+		confirmaSaldo(t, carteira, Bitcoin(10))
+	})
 }
