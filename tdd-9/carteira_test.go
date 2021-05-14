9304b30e7a138c0451e4d40142af491cdcb817eb
andre.luis <andre.luis@g.globo>
2021-05-13 22:08:05 -0300

    add circulo

--- tdd-9/carteira_test.go
+++ tdd-9/carteira_test.go
@@ -24,5 +24,17 @@ func TestCarteira(t *testing.T) {
 		carteira := Carteira{saldo: Bitcoin(20)}
 		carteira.Retirar(10)
 		confirmaSaldo(t, carteira, Bitcoin(10))
+
 	})
+
+	t.run("Retirar com saldo insuficiente", func(t *testing){
+		saldoInicial := Bitcoin(20)
+		carteira := Carteira{saldoInicial}
+		erro := carteira.Retirar(Bitcoin(100))
+
+		if erro == nil{
+			t.Error("Esperava um erro mas nenhum aconteceu")
+		}
+	
+	)	
 }
